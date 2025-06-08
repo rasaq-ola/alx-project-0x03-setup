051f9eeda42979e779629ab4bb08eeb75df507b5
Rasaq-ola <rasaqola196@gmail.com>
2025-06-08 03:10:58 +0100

    adding Button components and function

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,13 +1,13 @@
 import { useRouter } from 'next/router';
+import Button from '../components/common/Button';
 
-const Home = () => {
+export default function Home() {
   const router = useRouter();
 
   return (
     <div>
       <h1>Welcome to Splash App</h1>
+      <Button onClick={() => router.push('/about')}>Go to About Page</Button>
     </div>
   );
-};
-
-export default Home;
+}
